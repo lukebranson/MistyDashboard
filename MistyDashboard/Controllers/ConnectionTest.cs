a8a50e041e7ee2b689b58dd1bfbdbe7a622c90de
lukebranson <44709673+lukebranson@users.noreply.github.com>
2022-02-11 11:18:08 -0600

    sh: wq: command not found

---
+++ MistyDashboard/Controllers/ConnectionTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MistyDashboard.ApiControllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConnectionTest : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
---
+++ MistyDashboard/obj/Release/net6.0/PubTmp/Out/wwwroot/js/mistySkillStarter.js
@@ -0,0 +1,10 @@
+﻿function startMisty(botIP) {
+    botUrl = "http://" + botIP + "/api/skills/start";
+    skillData = { "Skill" : "Hello World" };
+    $.post({
+        url: botUrl, skillData, function(result) {
+            console.log(result);
+        }
+    });
+    console.log("misty log complete");
+}
